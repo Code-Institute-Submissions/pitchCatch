89c8fbb6d0d6ffef29915990f535f78e717feff1
Deasun OD <des_donn@mailbox.org>
2018-07-19 00:55:22 +0000

    added exception to catch integrity errors and flash messages for registration success/error

--- reg_views.py
+++ reg_views.py
@@ -3,6 +3,7 @@ from run import app
 from models import Pitcher, Pitch, Catcher, caught
 from forms import PitchForm, PitcherForm, CatcherForm
 from database import db_session
+from sqlalchemy.exc import IntegrityError
 
 """REGISTRATION VIEWS"""
 
@@ -14,22 +15,29 @@ def reg_pitcher():
     
     form = PitcherForm(request.form)
     
-    if request.method == 'POST' and form.validate():
-        pitcher = Pitcher(
-            form.movement_name.data,
-            form.movement_url.data,
-            form.movement_description.data,
-            form.interests.data,
-            form.email.data,
-            form.region.data,
-            )
-        db_session.add(pitcher)
-        db_session.commit()
+    if request.method == 'POST':
         
-        # need a KeyError exception here to avoid duplicates
-
-        # Change this to redirect to new profile page
-        return redirect(url_for('index'))
+        try: 
+            pitcher = Pitcher(
+                form.movement_name.data,
+                form.movement_url.data,
+                form.movement_description.data,
+                form.interests.data,
+                form.email.data,
+                form.region.data,
+                )
+            db_session.add(pitcher)
+            db_session.commit()
+        
+        except IntegrityError:
+            db_session.rollback()
+            
+            flash("Your movement name is already registered with pitchCatch!")
+            return render_template('reg_pitcher.html', form=form)
+        
+        movement_name=pitcher.movement_name
+        flash("Welcome to pitchCatch!")
+        return redirect(url_for('pitcher_profile', movement_name=movement_name))
 
     return render_template('reg_pitcher.html', form=form)
 
@@ -42,24 +50,34 @@ def reg_catcher():
     
     form = CatcherForm(request.form)
     
-    if request.method == 'POST' and form.validate():
-        catcher = Catcher(
-            form.developer_name.data,
-            form.developer_url.data,
-            form.developer_description.data,
-            form.interests.data,
-            form.frontend_interest.data,
-            form.backend_interest.data,
-            form.frontend_experience.data,
-            form.backend_experience.data,
-            form.prog_languages.data,
-            form.email.data,
-            form.region.data,
-            )
-        db_session.add(catcher)
-        db_session.commit()
-        return redirect(url_for('index'))
-
+    if request.method == 'POST':
+        try:           
+            catcher = Catcher(
+                form.developer_name.data,
+                form.developer_url.data,
+                form.developer_description.data,
+                form.interests.data,
+                form.frontend_interest.data,
+                form.backend_interest.data,
+                form.frontend_experience.data,
+                form.backend_experience.data,
+                form.prog_languages.data,
+                form.email.data,
+                form.region.data,
+                )
+            db_session.add(catcher)
+            db_session.commit()
+    
+        except IntegrityError:
+            db_session.rollback()
+                
+            flash("Your username is already registered with pitchCatch!")
+            return render_template('reg_catcher.html', form=form)
+        
+        developer_name=catcher.developer_name
+        flash("Welcome to pitchCatch!")
+        return redirect(url_for('catcher_profile', developer_name=developer_name))
+        
     return render_template('reg_catcher.html', form=form)
 
 
@@ -72,21 +90,28 @@ def reg_pitch():
     form = PitchForm(request.form)
 
     if request.method == 'POST':
+        try:
+            pitch = Pitch(
+                form.proposal_name.data,
+                form.proposal_outline.data,
+                form.interests.data,
+                form.launch_date.data,
+                form.pitcher_id.data
+                )
+            print(pitch.pitcher_id)
+            print(pitch.launch_date)
+            db_session.add(pitch)
+            db_session.commit()
+            
+        except IntegrityError:
+            db_session.rollback()
+                
+            flash("This pitch name is already registered with pitchCatch!")
+            return render_template('reg_pitch.html', form=form)
 
-        pitch = Pitch(
-            form.proposal_name.data,
-            form.proposal_outline.data,
-            form.interests.data,
-            form.launch_date.data,
-            form.pitcher_id.data
-            )
-        print(pitch.pitcher_id)
-        print(pitch.launch_date)
-        db_session.add(pitch)
-        db_session.commit()
-        
-        # Change this to redirect to an acknowledgement page
-        return redirect(url_for('index'))
+        proposal_name=pitch.proposal_name
+        flash("Your pitch is registered with pitchCatch!")
+        return redirect(url_for('pitch_profile', proposal_name=proposal_name))
 
     sponsor = db_session.query(Pitcher).all()
     return render_template('reg_pitch.html', form=form, sponsor=sponsor)
