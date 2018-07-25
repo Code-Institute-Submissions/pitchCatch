025e58a26f1d628c41463aa5efe3c804b05568b3
Deasun OD <des_donn@mailbox.org>
2018-07-25 22:17:11 +0000

    deleted _footer.scss file

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ from sqlalchemy import create_engine
 # imported by run.py
 DEBUG = True
 DATABASE_URI = create_engine('postgresql://ubuntu:pw123@localhost:5432/pitch_catch_caught', convert_unicode=True)
-SECRET_KEY = '17348hyutrm567'
+SECRET_KEY = '17348hyutrm765'
