7e795c48325f76a1e15ea2b22bd2621fdfaaa85d
Deasun OD <des_donn@mailbox.org>
2018-07-25 22:45:10 +0000

    amended DATABSE_URL

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
 # imported by run.py
 DEBUG = True
-DATABASE_URI = create_engine('postgresql://ubuntu:pw123@localhost:5432/pitch_catch_caught', convert_unicode=True)
+DATABASE_URL = create_engine('postgresql://ubuntu:pw123@localhost:5432/pitch_catch_caught', convert_unicode=True)
 SECRET_KEY = '17348hyutrm765'
