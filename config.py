12a6d54df6497a161b0177267a31843e84d85c35
Deasun OD <des_donn@mailbox.org>
2018-08-18 11:37:45 +0000

    untracked config.py from git and stored in .gitignore

--- config.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-import os
-
-# imported by run.py
-DEBUG = True
-DATABASE_URL = create_engine('postgres://fotesrlguspefq:a25708e19dbc87dd2f6fb91421acc77fe54f1b9713b4ff028fc828162912a6b8@ec2-54-228-251-254.eu-west-1.compute.amazonaws.com:5432/d1oe01f2fn7o0l', convert_unicode=True)
-SECRET_KEY = '17348hyutrm765'
-
-
